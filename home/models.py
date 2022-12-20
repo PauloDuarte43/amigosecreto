cefbab456e0156009be4c87330b2e4ceb50aa678
Paulo Duarte <p.roger.antunes43@gmail.com>
2022-12-20 06:47:08 +0000

    initial commit

---
+++ home/models.py
@@ -0,0 +1,20 @@
+import random
+import string
+
+from django.db import models
+
+
+def rand_str(): 
+    return ''.join(random.choice(string.ascii_lowercase) for i in range(6))
+
+
+class Friend(models.Model):
+    username = models.CharField(max_length=30, null=False)
+    name = models.CharField(max_length=200, null=False)
+    password = models.CharField(max_length=20, null=False, default=rand_str)
+    desired_gift = models.CharField(max_length=200, null=True, blank=True)
+    secret_friend = models.ForeignKey('Friend', on_delete=models.DO_NOTHING, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.username} - {self.password}'
+
---
+++ home/views.py
@@ -0,0 +1,55 @@
+import random
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.db.models import Q
+
+from .models import Friend
+
+
+def index(request):
+    msg = ''
+    if request.method == "POST":
+        if request.POST.get('username') and request.POST.get('password'):
+            username = request.POST['username']
+            friend = Friend.objects.get(
+                    Q(username=username)|Q(name=username),
+                    password=request.POST['password'])
+
+            if friend:
+                if request.POST.get('gift'):
+                    friend.desired_gift = request.POST['gift']
+
+                if not friend.secret_friend:
+                    _fns = Friend.objects.filter(
+                        secret_friend__isnull=False
+                    )
+
+                    if _fns:
+                        _rnd_friend = Friend.objects.exclude(id=friend.id, id__in=[i.id for i in _fns])
+                    else:
+                        _rnd_friend = Friend.objects.exclude(id=friend.id)
+
+                    friend.secret_friend = random.choice(_rnd_friend)
+
+                friend.save()
+
+            else:
+                msg = 'Usuário ou senha inválidos!'
+        else:
+            msg = 'Usuário ou senha inválidos!'
+
+    friend_no_secret = Friend.objects.exclude(
+        secret_friend__isnull=False
+    ).order_by('username')
+
+    friend_desired_gift = Friend.objects.filter(
+        desired_gift__isnull=False
+    ).order_by('username')
+
+    context = {
+        'message': msg,
+        'friend_no_secret': friend_no_secret,
+        'friend_desired_gift': friend_desired_gift
+    }
+
+    return render(request, 'home/index.html', context)
