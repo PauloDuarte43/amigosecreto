5bfef549969518954fd74af49142cc4901f26937
Paulo Duarte <p.roger.antunes43@gmail.com>
2024-12-16 00:50:07 -0300

    Fix

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,30 @@
 from django.contrib import admin
-
+from django.http import HttpResponse
 from .models import Friend
+from urllib.parse import urlencode
 
-admin.site.register(Friend)
+def generate_login_urls(modeladmin, request, queryset):
+    """
+    Gera URLs de login automático para os usuários selecionados
+    """
+    base_url = request.build_absolute_uri('/?')
+    urls = []
+    
+    for friend in queryset:
+        params = urlencode({'username': friend.username, 'password': friend.password})
+        url = f"{base_url}{params}"
+        urls.append(url)
+    
+    response_content = "\n".join(urls)
+    
+    # Retorna como texto para download
+    response = HttpResponse(response_content, content_type="text/plain")
+    response['Content-Disposition'] = 'attachment; filename="login_urls.txt"'
+    return response
+
+generate_login_urls.short_description = "Gerar URLs de login automático"
+
+@admin.register(Friend)
+class FriendAdmin(admin.ModelAdmin):
+    list_display = ('username', 'name', 'password', 'secret_friend')
+    actions = [generate_login_urls]
--- home/views.py
+++ home/views.py
@@ -9,54 +9,56 @@ from .models import Friend
 def index(request):
     msg = ''
     logged_friend = None
-    if request.method == "POST":
-        if request.POST.get('username') and request.POST.get('password'):
-            username = request.POST['username']
-            try:
-                friend = Friend.objects.get(
-                        Q(username=username)|Q(name=username),
-                        password=request.POST['password'])
-            except:
-                friend = None
 
-            if friend:
-                logged_friend = friend
-                if request.POST.get('gift'):
-                    friend.desired_gift = request.POST['gift']
+    # Obtendo username e password de POST ou GET
+    username = request.POST.get('username') or request.GET.get('username')
+    password = request.POST.get('password') or request.GET.get('password')
 
-                if not friend.secret_friend:
-                    _fns = Friend.objects.filter(
-                        secret_friend__isnull=False
-                    )
+    if username and password:
+        try:
+            friend = Friend.objects.get(
+                Q(username=username) | Q(name=username),
+                password=password
+            )
+        except Friend.DoesNotExist:
+            friend = None
 
-                    if _fns:
-                        _fns = [i.secret_friend.id for i in _fns]
-                        print(_fns)
-                        print('alguns amigos')
-                        _rnd_friend = Friend.objects.exclude(id=friend.id).exclude(id__in=_fns)
+        if friend:
+            logged_friend = friend
+            if request.method == "POST" and request.POST.get('gift'):
+                friend.desired_gift = request.POST['gift']
+
+            if not friend.secret_friend:
+                _fns = Friend.objects.filter(secret_friend__isnull=False)
+
+                if _fns:
+                    _fns = [i.secret_friend.id for i in _fns]
+                    print(_fns)
+                    print('alguns amigos')
+                    _rnd_friend = Friend.objects.exclude(id=friend.id).exclude(id__in=_fns)
+                else:
+                    print('qualquer amigo')
+                    _rnd_friend = Friend.objects.exclude(id=friend.id)
+
+                print(_rnd_friend)
+                if _rnd_friend:
+                    if len(_rnd_friend) == 2:
+                        _choice = _rnd_friend[0]
+                        if Friend.objects.exclude(
+                                id=friend.id).exclude(
+                                    id__in=_fns).exclude(
+                                        id=_rnd_friend[0].id)[0].id == _rnd_friend[1].id:
+                            print('Regra troca')
+                            _choice = _rnd_friend[1]
                     else:
-                        print('qualquer amigo')
-                        _rnd_friend = Friend.objects.exclude(id=friend.id)
-                    print(_rnd_friend)
-                    if _rnd_friend:
-                        if len(_rnd_friend) == 2:
-                            _choice = _rnd_friend[0]
-                            if Friend.objects.exclude(
-                                    id=friend.id).exclude(
-                                            id__in=_fns).exclude(
-                                                    id=_rnd_friend[0].id)[0].id == _rnd_friend[1].id:
-                                print('Regra troca')
-                                _choice = _rnd_friend[1]
-                        else:
-                            _choice = random.choice(_rnd_friend)
-                        #_choice = random.choice(_rnd_friend)
+                        _choice = random.choice(_rnd_friend)
 
-                        friend.secret_friend = _choice
-                friend.save()
-            else:
-                msg = 'Usuário ou senha inválidos!'
+                    friend.secret_friend = _choice
+            friend.save()
         else:
             msg = 'Usuário ou senha inválidos!'
+    else:
+        msg = 'Usuário ou senha inválidos!'
 
     friend_no_secret = Friend.objects.exclude(
         secret_friend__isnull=False
@@ -70,7 +72,9 @@ def index(request):
         'logged_friend': logged_friend,
         'message': msg,
         'friend_no_secret': friend_no_secret,
-        'friend_desired_gift': friend_desired_gift
+        'friend_desired_gift': friend_desired_gift,
+        'username': username or '',
+        'password': password or ''
     }
 
     return render(request, 'home/index.html', context)
